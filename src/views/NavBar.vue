b69ef6daf0506b53af68984cc95f676f19572ed5
Nicolas Delbovier <nicolas.delbovier@soundnodes.com>
2024-12-07 20:55:25 +0100

    Create a simple nav bar

---
+++ src/views/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div class="navbar-container">
+        <NavBarItem imgSrc="../src/assets/gameController.svg" width="50" height="50" text="Jouer" />
+        <NavBarItem imgSrc="../src/assets/newPage.svg" width="50" height="50" text="Thèmes" />
+        <NavBarItem imgSrc="../src/assets/add.svg" width="50" height="50" text="créer" />
+    </div>
+
+</template>
+
+<script>
+import NavBarItem from './NavBarItem.vue'
+
+export default {
+    components: {
+        NavBarItem
+    }
+}
+</script>
+
+<style scoped>
+.navbar-container{
+    position: sticky;
+    bottom:0;
+    border-radius: 5%;
+    box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
+    padding: 20px;
+    margin: 20px auto;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ src/views/NavBarItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <button>
+        <img alt="Vue logo" class="logo" :src="imgSrc" :width="width" :height="height" />
+        <span class="">{{ text }}</span>
+    </button>
+</template>
+
+<script>
+export default {
+    props: ['imgSrc', 'width', 'height', 'text']
+
+}
+</script>
+
+<style scoped>
+button{
+    background: none;
+    border: none;
+}
+img {
+    display: block;
+    fill: blue;
+}
+</style>
