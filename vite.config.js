824c764976db8a19c2983722c0dd9210dc2bf2e5
Nicolas Delbovier <ndelbovier26@gmail.com>
2025-05-12 20:38:57 +0200

    Install tailwind

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
     VitePWA({
