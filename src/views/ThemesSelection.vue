cf23149d394909c90e7eb48914efe9165f5e178f
Nicolas Delbovier <ndelbovier26@gmail.com>
2026-05-19 16:44:51 +0200

    [IMP] ThemesSelection: Make "new" button trigger editTheme view

--- src/views/ThemesSelection.vue
+++ src/views/ThemesSelection.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { inject } from 'vue';
 import ThemeCard from '@/components/ThemeCard.vue';
 
 const props = defineProps(['decks', 'selectedThemes']);
 const emit = defineEmits(['useTheme']);
+
+const triggerEditTheme = inject('triggerEditTheme');
 </script>
 
 <template>
@@ -18,6 +21,7 @@ const emit = defineEmits(['useTheme']);
 
     <button
       class="border-2 border-dashed border-text-color grid grid-rows-[1fr_auto] justify-items-center items-center text-text-color cursor-pointer p-4 rounded-2xl w-11/12 gap-2"
+      @click="triggerEditTheme('New')"
     >
       <p class="text-center text-lg font-bold">Nouveau Thème</p>
       <img
